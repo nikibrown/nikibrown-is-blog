7ea06ea0c8e4214477c5dc95e0e220734097aaf8
Niki Brown <284173+nikibrown@users.noreply.github.com>
2024-09-14 19:40:17 -0700

    dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,17 @@
-import type { Config } from "tailwindcss";
-import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss"
+import typography from "@tailwindcss/typography"
 
 export default {
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./lib/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-inter)"],
-      },
-    },
-  },
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  plugins: [typography],
-} satisfies Config;
+	content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-inter)"],
+			},
+		},
+	},
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	plugins: [typography],
+} satisfies Config
