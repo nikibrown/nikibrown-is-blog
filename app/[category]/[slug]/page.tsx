e27660633ac14cf6d0268fe23c3f8fa7496f8002
Niki Brown <284173+nikibrown@users.noreply.github.com>
2024-09-17 11:15:46 -0700

    remove . from title

--- app/[category]/[slug]/page.tsx
+++ app/[category]/[slug]/page.tsx
@@ -27,7 +27,6 @@ export default async function PostPage({ params }: { params: { category: string;
 				<Link href="/" className="hover:underline">
 					Blog
 				</Link>
-				.
 			</h2>
 			<article>
 				<h1 className="mb-12 text-center text-6xl font-bold leading-tight tracking-tighter md:text-left md:text-7xl md:leading-none lg:text-8xl">
